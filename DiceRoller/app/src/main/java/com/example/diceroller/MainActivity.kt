a0834ef39c555f29b837f41d85a17238a4242bb5
Jakub Wandelt <234772@edu.p.lodz.pl>
2022-10-19 20:49:32 +0200

    Implemented rolling with use of accelerometer

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.diceroller
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lateinit var sensorManager: SensorManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
@@ -23,5 +37,18 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
         Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val accelerometer: TextView = findViewById(R.id.accelerometer_data)
+        accelerometer.text = "x = ${event!!.values[0]}\n\n" +
+                "y = ${event.values[1]}\n\n" +
+                "z = ${event.values[2]}"
+        if (event.values[0] >= 10) {
+            rollDice()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 }
