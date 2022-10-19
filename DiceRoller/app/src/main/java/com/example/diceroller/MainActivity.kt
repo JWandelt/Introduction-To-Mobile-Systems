431e6866d7250093ce1546e930bfc24336b3e855
Jakub Wandelt <234772@edu.p.lodz.pl>
2022-10-19 22:37:27 +0200

    Added images to the dices

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private val dices = arrayOf("1", "2", "3")
     var numberOfDices: Int = 1
 
+    val diceMap = mapOf(1 to "dice_1", 2 to "dice_2", 3 to "dice_3", 4 to "dice_4", 5 to "dice_5", 6 to "dice_6")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -61,24 +63,40 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val dice1 = Dice(6)
         val dice2 = Dice(6)
         val dice3 = Dice(6)
+        val diceImage1: ImageView = findViewById(R.id.first_dice)
+        val diceImage2: ImageView = findViewById(R.id.second_dice)
+        val diceImage3: ImageView = findViewById(R.id.third_dice)
+        var mapValue: String
         when (dicesToRoll) {
             1 -> {
                 val diceRoll1 = dice1.roll()
                 resultTextView1.text = diceRoll1.toString()
+                mapValue = diceMap.get(diceRoll1)!!
+                diceImage1.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
             }
             2 -> {
                 val diceRoll1 = dice1.roll()
                 resultTextView1.text = diceRoll1.toString()
+                mapValue = diceMap.get(diceRoll1)!!
+                diceImage1.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
                 val diceRoll2 = dice2.roll()
                 resultTextView2.text = diceRoll2.toString()
+                mapValue = diceMap.get(diceRoll2)!!
+                diceImage2.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
             }
             3 -> {
                 val diceRoll1 = dice1.roll()
                 resultTextView1.text = diceRoll1.toString()
+                mapValue = diceMap.get(diceRoll1)!!
+                diceImage1.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
                 val diceRoll2 = dice2.roll()
                 resultTextView2.text = diceRoll2.toString()
+                mapValue = diceMap.get(diceRoll2)!!
+                diceImage2.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
                 val diceRoll3 = dice3.roll()
                 resultTextView3.text = diceRoll3.toString()
+                mapValue = diceMap.get(diceRoll3)!!
+                diceImage3.setImageResource(this.resources.getIdentifier(mapValue, "drawable", this.packageName))
             }
         }
     }
