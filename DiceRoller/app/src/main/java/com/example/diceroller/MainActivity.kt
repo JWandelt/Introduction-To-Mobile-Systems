31f2e3184244bdff06dd5f44ebdd9b12658c6083
Jakub Wandelt <234772@edu.p.lodz.pl>
2022-10-19 17:30:10 +0200

    Test

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +22,6 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 }
